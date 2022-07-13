0f97a314c11e25f37beac1ca286e6963631d46de
RyanKoech <sirryankoech@gmail.com>
2022-07-13 12:10:12 +0300

    Implemented popular_food_detail page body

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/dimensions.dart';
+import 'package:food_delivery/widgets/main_info_column.dart';
 
+import '../../utils/colors.dart';
 import '../../widgets/app_icon.dart';
+import '../../widgets/big_text.dart';
+import '../../widgets/icon_add_text.dart';
+import '../../widgets/small_text.dart';
 
 class PopularFoodDetail extends StatelessWidget {
   const PopularFoodDetail({Key? key}) : super(key: key);
@@ -9,6 +14,7 @@ class PopularFoodDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Stack(
         children: [
           Positioned(
@@ -35,7 +41,84 @@ class PopularFoodDetail extends StatelessWidget {
               ],
             ),
           ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: Dimensions.size350 - 20,
+            child: Container(
+              padding: EdgeInsets.only(
+                left: Dimensions.size20,
+                right: Dimensions.size20,
+                top: Dimensions.size20,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(Dimensions.size20),
+                ),
+                color: Colors.white,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  MainInfoColumn(
+                    text: "Chinese Side",
+                  ),
+                  SizedBox(
+                    height: Dimensions.size20,
+                  ),
+                  BigText(text: "Introduce")
+                ],
+              ),
+            ),
+          )
         ],
+      ),
+      bottomNavigationBar: Container(
+        height: Dimensions.size120,
+        padding: EdgeInsets.all(Dimensions.size20),
+        decoration: BoxDecoration(
+          color: AppColors.buttonBackgroundColor,
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(Dimensions.size20 * 2),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              padding: EdgeInsets.all(Dimensions.size20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimensions.size20),
+                color: Colors.white,
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.remove, color: AppColors.signColor),
+                  SizedBox(
+                    width: Dimensions.size5,
+                  ),
+                  BigText(text: "0"),
+                  SizedBox(
+                    width: Dimensions.size5,
+                  ),
+                  Icon(Icons.add, color: AppColors.signColor)
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(Dimensions.size20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimensions.size20),
+                color: AppColors.mainColor,
+              ),
+              child: BigText(
+                text: "\$10 | Add to Cart",
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
