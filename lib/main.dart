e8bcc37c4ce04b1a7951bfccbf7e0dd4691bbc85
RyanKoech <sirryankoech@gmail.com>
2022-07-16 09:46:48 +0300

    Called init function in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/pages/home/main_food_page.dart';
 import 'package:get/get.dart';
+import 'helper/dependencies.dart' as dep;
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(const MyApp());
 }
 
