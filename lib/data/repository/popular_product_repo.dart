200f4adea0832a32d09027b3e861111b51b1969d
RyanKoech <sirryankoech@gmail.com>
2022-07-17 17:34:56 +0300

    Utilized defined app constants

--- lib/data/repository/popular_product_repo.dart
+++ lib/data/repository/popular_product_repo.dart
@@ -1,4 +1,5 @@
 import 'package:food_delivery/data/api/api_client.dart';
+import 'package:food_delivery/utils/constants.dart';
 import 'package:get/get.dart';
 
 class PopularProductRepo extends GetxService {
@@ -6,6 +7,6 @@ class PopularProductRepo extends GetxService {
   PopularProductRepo({required this.apiClient});
 
   Future<Response> getPopularProductList() async {
-    return await apiClient.getData("uri");
+    return await apiClient.getData(Constants.POPULAR_FOODS_URI);
   }
 }
--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,11 +1,12 @@
 import 'package:food_delivery/controller/popular_product_controller.dart';
 import 'package:food_delivery/data/api/api_client.dart';
 import 'package:food_delivery/data/repository/popular_product_repo.dart';
+import 'package:food_delivery/utils/constants.dart';
 import 'package:get/get.dart';
 
 Future<void> init() async {
   //Api Client
-  Get.lazyPut(() => ApiClient(appBaseUrl: ""));
+  Get.lazyPut(() => ApiClient(appBaseUrl: Constants.BASE_URL));
 
   //Repositories
   Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
