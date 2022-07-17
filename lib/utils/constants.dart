40cab56ec47039472bbbc9fc783e0090ba7f8b16
RyanKoech <sirryankoech@gmail.com>
2022-07-17 17:33:22 +0300

    Added constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,10 @@
+class Constants {
+  static const String APP_NAME = "FoodApp";
+  static const String APP_VERSION = "1.0.0";
+
+  static const String BASE_URL = "http://mvs.bslmeiyu.com/api/v1/products/";
+  static const String POPULAR_FOODS_URI = "popular";
+  static const String RECOMMENDED_FOODS_URI = "recommended";
+
+  static const String TOKEN = "dbToken";
+}
