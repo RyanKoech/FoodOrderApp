1489ed6a0039b0d9161e6e7fce99f29f2e2ea55d
RyanKoech <sirryankoech@gmail.com>
2022-07-17 17:33:00 +0300

    Utilized the products datamodels

--- lib/controller/popular_product_controller.dart
+++ lib/controller/popular_product_controller.dart
@@ -1,6 +1,8 @@
 import 'package:food_delivery/data/repository/popular_product_repo.dart';
 import 'package:get/get.dart';
 
+import '../model/popular_products_model.dart';
+
 class PopularProductController extends GetxController {
   final PopularProductRepo popularProductRepo;
 
@@ -13,7 +15,7 @@ class PopularProductController extends GetxController {
     Response response = await popularProductRepo.getPopularProductList();
     if (response.statusCode == 200) {
       _popularProductList = [];
-      // _popularProductList.addAll();
+      _popularProductList.addAll(Product.fromJson(response.body).product);
       update();
     } else {}
   }
