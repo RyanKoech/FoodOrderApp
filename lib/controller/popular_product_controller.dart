6ada191147176603a5247df86dff133014e2a963
RyanKoech <sirryankoech@gmail.com>
2022-07-18 08:40:11 +0300

    Added new varaible to track loading state

--- lib/controller/popular_product_controller.dart
+++ lib/controller/popular_product_controller.dart
@@ -10,10 +10,14 @@ class PopularProductController extends GetxController {
 
   List<dynamic> _popularProductList = [];
   List<dynamic> get popularProductList => _popularProductList;
+  bool _isLoaded = false;
+  bool get isLoaded => _isLoaded;
 
   Future<void> getPopularProductList() async {
     Response response = await popularProductRepo.getPopularProductList();
     if (response.statusCode == 200) {
+      _isLoaded = true;
+      print("Products found");
       _popularProductList = [];
       _popularProductList.addAll(Product.fromJson(response.body).product);
       update();
