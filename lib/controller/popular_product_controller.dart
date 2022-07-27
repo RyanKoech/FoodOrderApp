016a9c1384e8e966f0a3068d8120428f52133e39
RyanKoech <sirryankoech@gmail.com>
2022-07-27 10:03:37 +0300

    Implemented logic to change quanity in bottom navigation

--- lib/controller/popular_product_controller.dart
+++ lib/controller/popular_product_controller.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:food_delivery/data/repository/popular_product_repo.dart';
+import 'package:food_delivery/utils/colors.dart';
 import 'package:get/get.dart';
 
 import '../model/popular_products_model.dart';
@@ -12,6 +14,10 @@ class PopularProductController extends GetxController {
   List<dynamic> get popularProductList => _popularProductList;
   bool _isLoaded = false;
   bool get isLoaded => _isLoaded;
+  int _quantity = 0;
+  int get quantity => _quantity;
+  int _inCartItems = 0;
+  int get inCarItems => _inCartItems + _quantity;
 
   Future<void> getPopularProductList() async {
     Response response = await popularProductRepo.getPopularProductList();
@@ -22,5 +28,34 @@ class PopularProductController extends GetxController {
       _popularProductList.addAll(Product.fromJson(response.body).product);
       update();
     } else {}
+  }
+
+  void setQuantity(bool isIncrement) {
+    if (isIncrement) {
+      _quantity = checkQuantity(++_quantity);
+    } else {
+      _quantity = checkQuantity(--_quantity);
+    }
+    update();
+  }
+
+  int checkQuantity(int quantity) {
+    if (quantity < 0) {
+      Get.snackbar("Item count", "You can't reduce more!",
+          backgroundColor: AppColors.mainColor, colorText: Colors.white);
+      return 0;
+    } else if (quantity > 20) {
+      Get.snackbar("Item count", "You can't add more!",
+          backgroundColor: AppColors.mainColor, colorText: Colors.white);
+      return 20;
+    } else {
+      return quantity;
+    }
+  }
+
+  void initProduct() {
+    _quantity = 0;
+    _inCartItems = 0;
+    //Get from storage
   }
 }
